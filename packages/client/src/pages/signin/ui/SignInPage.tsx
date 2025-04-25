be2ce618d2c1c0b1217231d42ebe23bb51c67ea2
Sesh <sergeimorozov212001@gmail.com>
2025-04-25 18:42:41 +0300

    feat(added validators for signin)

--- packages/client/src/pages/signin/ui/SignInPage.tsx
+++ packages/client/src/pages/signin/ui/SignInPage.tsx
@@ -3,69 +3,94 @@ import styles from './SignInPage.module.scss';
 import { Input } from '@/shared/ui/Input';
 import { Button } from '@/shared/ui/Button';
 import { BrowserRouter, Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { useFormValidator } from '@/shared/validators/FormValidator';
+import { validationTemplate } from '@/shared/validators/Rules';
 
 export const SignInPage = memo(() => {
+  const {
+    register,
+    handleSubmit,
+    trigger,
+    getValues,
+    formState: { errors }
+  } = useForm({
+    mode: 'onBlur'
+  });
+
+  const onSubmit = (data: any) => {
+    console.log('данные формы:', data);
+  };
+
   return (
     <main className={styles.signin}>
       <section className={styles.signin__panel}>
         <div className={styles.signin__title}>Регистрация</div>
-        <form className={styles.signin__form}>
+        <form
+          className={styles.signin__form}
+          onSubmit={handleSubmit(onSubmit)}>
           <Input
             className={styles.signin__field}
             label={'Почта'}
-            name={'email'}
-            required={true}
-            handleChange={handleChange}
+            {...register('email', useFormValidator(validationTemplate(getValues).email))}
+            isInvalid={!!errors.email}
+            error={errors.email?.message as string}
+            onFocus={() => trigger('email')}
           />
           <Input
             className={styles.signin__field}
             label={'Логин'}
-            name={'login'}
-            required={true}
-            handleChange={handleChange}
+            {...register('login', useFormValidator(validationTemplate(getValues).login))}
+            isInvalid={!!errors.login}
+            error={errors.login?.message as string}
+            onFocus={() => trigger('login')}
           />
           <Input
             className={styles.signin__field}
             label={'Имя'}
-            name={'first_name'}
-            required={true}
-            handleChange={handleChange}
+            {...register('first_name', useFormValidator(validationTemplate(getValues).first_name))}
+            isInvalid={!!errors.first_name}
+            error={errors.first_name?.message as string}
+            onFocus={() => trigger('first_name')}
           />
           <Input
             className={styles.signin__field}
             label={'Фамилия'}
-            name={'second_name'}
-            required={true}
-            handleChange={handleChange}
+            {...register('second_name', useFormValidator(validationTemplate(getValues).second_name))}
+            isInvalid={!!errors.second_name}
+            error={errors.second_name?.message as string}
+            onFocus={() => trigger('second_name')}
           />
           <Input
             className={styles.signin__field}
             label={'Телефон'}
-            name={'phone'}
-            required={true}
-            handleChange={handleChange}
+            {...register('phone', useFormValidator(validationTemplate(getValues).phone))}
+            isInvalid={!!errors.phone}
+            error={errors.phone?.message as string}
+            onFocus={() => trigger('phone')}
           />
           <Input
             className={styles.signin__field}
             label={'Пароль'}
-            name={'password'}
-            type="password"
-            required={true}
-            handleChange={handleChange}
+            {...register('password', useFormValidator(validationTemplate(getValues).password))}
+            isInvalid={!!errors.password}
+            type={'password'}
+            error={errors.password?.message as string}
+            onFocus={() => trigger('password')}
           />
           <Input
             className={styles.signin__field}
             label={'Пароль (еще раз)'}
-            name={'password_two'}
-            type="password"
-            required={true}
-            handleChange={handleChange}
+            {...register('password_two', useFormValidator(validationTemplate(getValues).password_two))}
+            isInvalid={!!errors.password_two}
+            type={'password'}
+            error={errors.password_two?.message as string}
+            onFocus={() => trigger('password_two')}
           />
           <Button
             className={styles.signin__button}
             type={'submit'}
-            name={'Авторизироваться'}
-            handleClick={handleClick}></Button>
+            name={'Авторизироваться'}></Button>
           <BrowserRouter>
             <Link
               className={styles.signin__link}
@@ -78,11 +103,3 @@ export const SignInPage = memo(() => {
     </main>
   );
 });
-
-const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  return 0;
-};
-
-const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-  return 0;
-};
