350e25f29589dad439987179ef75b9e4472bcc24
Sesh <sergeimorozov212001@gmail.com>
2025-04-27 13:50:16 +0300

    fix(fixed new styles)

--- packages/client/src/shared/ui/Input/Input.model.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
-  label: string;
-  isInvalid?: boolean;
-  type?: 'text' | 'password';
-  error?: string;
-};
