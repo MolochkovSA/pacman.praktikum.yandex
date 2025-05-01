edd668a0d5abfaff3cdfb1af9a9d40b941ef83ab
Sesh <sergeimorozov212001@gmail.com>
2025-05-01 16:58:19 +0300

    feat(updated routes)

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,5 +1,13 @@
+import { TextEncoder, TextDecoder } from 'util';
+global.TextEncoder = TextEncoder;
+// @ts-expect-error
+global.TextDecoder = TextDecoder;
+
 import App from './App';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
 
 const appContent = 'Вход';
 
@@ -7,6 +15,11 @@ const appContent = 'Вход';
 global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve('hey') }));
 
 test('Example test', async () => {
-  render(<App />);
+  render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+  const r = document.textContent;
   expect(screen.getByText(appContent)).toBeDefined();
 });
