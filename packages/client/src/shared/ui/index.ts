6f40010d8ec2d91e0cea05922cdc2b5aa3148782
Sergey Molochkov <wwwsergwww@mail.ru>
2025-07-04 16:24:04 +0300

    fix: Modal

--- packages/client/src/shared/ui/index.ts
+++ packages/client/src/shared/ui/index.ts
@@ -3,5 +3,5 @@ export { Input } from './Input/Input';
 export { IconLink } from './IconLink/IconLink';
 export { Breadcrumbs } from './Breadcrumbs/Breadcrumbs';
 export { Spinner } from './Spinner/Spinner';
-export { Modal } from './Modal/Modal1';
+export { Modal } from './Modal/CustomModal';
 export { ErrorMessage } from './Error/Error';
