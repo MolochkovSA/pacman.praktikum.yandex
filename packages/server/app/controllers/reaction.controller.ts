f53d8139a349262d6c358f4c46fbd372b09df47e
Sergey Molochkov <wwwsergwww@mail.ru>
2025-07-06 16:29:42 +0300

    feat:  update server side of reaction

--- packages/server/app/controllers/reaction.controller.ts
+++ packages/server/app/controllers/reaction.controller.ts
@@ -1,34 +1,27 @@
 import { Request, Response } from 'express';
 import { ReactionService } from '../services/reaction.service';
+import { CreateReactionDto, DeleteReactionDto } from '../dto/reaction.dto';
 
 const reactionService = new ReactionService();
 
 export const getAllReaction = async (_: Request, res: Response) => {
   const reactions = await reactionService.getAllReactions();
 
-  return res.status(201).json(reactions);
+  return res.status(200).json(reactions);
 };
 
 export const addReactionByComment = async (req: Request, res: Response) => {
-  const { commentId, reactionId, author } = req.query as { commentId: string; reactionId: string; author: string };
+  const { commentId, reactionId, author } = req.body as CreateReactionDto;
 
-  await reactionService.setReaction(Number(commentId), Number(reactionId), author);
-
-  return res.status(201).json();
-};
-
-export const addReaction = async (req: Request, res: Response) => {
-  const { emoji } = req.body;
-
-  await reactionService.addReaction(emoji);
+  await reactionService.setReaction(commentId, reactionId, author);
 
   return res.status(201).json();
 };
 
 export const deleteReactionByComment = async (req: Request, res: Response) => {
-  const { commentId, reactionId } = req.query as { commentId: string; reactionId: string };
+  const { commentId, reactionId } = req.body as DeleteReactionDto;
 
-  await reactionService.deleteReaction(Number(commentId), Number(reactionId));
+  await reactionService.deleteReaction(commentId, reactionId);
 
-  return res.status(201).json();
+  return res.status(204).json();
 };
--- packages/server/app/dto/reaction.dto.ts
+++ packages/server/app/dto/reaction.dto.ts
@@ -1,6 +1,17 @@
+export interface CreateReactionDto {
+  commentId: number;
+  reactionId: number;
+  author: string;
+}
+
 export interface ReactionDto {
   commentId: number;
   reactionId: number;
   author: string;
   createdAt: string;
+}
+
+export interface DeleteReactionDto {
+  commentId: number;
+  reactionId: number;
 }
