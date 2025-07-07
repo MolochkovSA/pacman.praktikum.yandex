8953f90506cac2e2f00c98bfb9e1dac2402c84c5
antonina.m <antonina.m@itfox-web.com>
2025-07-08 00:08:56 +0300

    fix: av

--- packages/client/src/pages/forum-topic-view/ui/MessageBlock/MessageBlock.tsx
+++ packages/client/src/pages/forum-topic-view/ui/MessageBlock/MessageBlock.tsx
@@ -1,8 +1,8 @@
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 
-// import { getProfilePath } from '@/shared/lib/router';
-// import { getAvatarSrc } from '@/shared/lib/getAvatarSrc';
+import { getProfilePath } from '@/shared/lib/router';
+import { getAvatarSrc } from '@/shared/lib/getAvatarSrc';
 import { CommentId } from '@/entities/comment';
 import { CommentReaction } from '@/features/rection';
 import { MessageType } from '../../model/types';
@@ -25,13 +25,11 @@ export const MessageBlock = ({ type, id, themeDescription, text, createdAt, auth
     <Card>
       <Card.Body className={styles.messageBlock}>
         <div className={styles.author}>
-          <div>{author}</div>
-
-          {/* <Link to={getProfilePath(authorId)}>{author}</Link>
+          <Link to={getProfilePath(author)}>{author}</Link>
           <img
-            src={getAvatarSrc(avatar)}
-            alt={`avatar of ${login}`}
-          /> */}
+            src={getAvatarSrc(null)}
+            alt={`avatar of ${author}`}
+          />
         </div>
 
         <div className={styles.separator}></div>
