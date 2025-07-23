745462bbba2a6b03eff3209ac0ade3629cf7d142
Viktoria <colosova28@yandex.ru>
2025-07-23 22:27:21 +0300

    att 3

--- packages/client/src/shared/const/api.ts
+++ packages/client/src/shared/const/api.ts
@@ -5,6 +5,6 @@ const host: string = import.meta.env.VITE_SERVER_HOST;
 const port: string = import.meta.env.VITE_SERVER_PORT;
 
 // Если режим разработки, используем localhost:port, иначе отправляем все запросы на nginx, а тот в свою очередь маршрутизирует их
-export const PACMAN_API_URL: string = `/api/v2`;
-// export const PACMAN_API_URL: string =
-//   import.meta.env.VITE_MODE === 'development' ? `${host}:${port}/api/v2` : `/api/v2`;
+// export const PACMAN_API_URL: string = `/api/v2`;
+export const PACMAN_API_URL: string =
+  import.meta.env.VITE_MODE === 'development' ? `${host}:${port}/api/v2` : `/api/v2`;
