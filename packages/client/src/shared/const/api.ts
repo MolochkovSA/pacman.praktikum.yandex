26dea2860a3d7d5548d48bb02c4ba5a6c1ad20ad
Sergey Molochkov <wwwsergwww@mail.ru>
2025-07-30 17:49:59 +0300

    fix: VITE_YANDEX_REDIRECT_URL

--- packages/client/src/shared/const/api.ts
+++ packages/client/src/shared/const/api.ts
@@ -1,3 +1,4 @@
+const isDev = import.meta.env.VITE_MODE;
 export const API_PATH: string = import.meta.env.VITE_API_PATH;
 export const YANDEX_API_URL: string = import.meta.env.VITE_YANDEX_API_URL;
-export const YANDEX_REDIRECT_URL: string = import.meta.env.VITE_YANDEX_REDIRECT_URL;
+export const YANDEX_REDIRECT_URL: string = isDev ? 'http://localhost:3000' : import.meta.env.VITE_YANDEX_REDIRECT_URL;
